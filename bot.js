2415d73f6141efe7a6d6a895284956c73f4397bb
AceOf5pades <36089338+AceOf5pades@users.noreply.github.com>
2018-03-09 15:31:38 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const Discord = require("discord.js");
 // this is what we're refering to. Your client.
 const client = new Discord.Client();
 
-const prefix = "at!"
+const config.prefix = "at!"
 
 client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
